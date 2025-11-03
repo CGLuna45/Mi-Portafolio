c21fef323c3d9c8aba7767189deeefe012401651
CGLuna45 <carlosgustavo.2004@gmail.com>
2025-11-02 19:23:08 -0600

    si

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://CGLuna45.gtihub.io/Mi-Portafolio/',
+  base: 'https://CGLuna45.github.io/Mi-Portafolio/',
   build: {
     minify: 'esbuild',
     cssMinify: true,
